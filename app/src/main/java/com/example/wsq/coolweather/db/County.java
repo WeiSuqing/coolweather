6156284f58a37f71e6a53b0f66febc14a1b1b19c
WeiSuqing <1265704183@qq.com>
2017-05-25 16:10:19 +0800

    First commit

---
+++ app/src/main/java/com/example/wsq/coolweather/db/County.java
@@ -0,0 +1,8 @@
+package com.example.wsq.coolweather.db;
+
+/**
+ * Created by wsq on 2017/5/23.
+ */
+
+public class County {
+}
---
+++ app/src/main/java/com/example/wsq/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.wsq.coolweather.db;
+
+/**
+ * Created by wsq on 2017/5/23.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private String provinceCode;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
