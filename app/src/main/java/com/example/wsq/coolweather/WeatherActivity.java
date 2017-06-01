1d287f360a4243f26666802f0a94fc61a7e7aa90
WeiSuqing <1265704183@qq.com>
2017-06-01 10:35:09 +0800

    新增显示天气，切换城市和手动更新天气的功能。

--- app/src/main/java/com/example/wsq/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/wsq/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.wsq.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -20,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.wsq.coolweather.Service.AutoUpdateService;
 import com.example.wsq.coolweather.gson.Forecast;
 import com.example.wsq.coolweather.gson.Weather;
 import com.example.wsq.coolweather.util.HttpUtil;
@@ -54,9 +56,9 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //背景图与状态栏融合
-        if (Build.VERSION.SDK_INT >= 21) {
+        if (Build.VERSION.SDK_INT  >= 21) {
             View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
@@ -191,6 +193,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent=new Intent(this,AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
